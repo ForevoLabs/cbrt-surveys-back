78401c01577ac9701c4cbe199443014947e36218
Andrey Demin <fkn801@gmail.com>
2019-09-28 17:46:01 +0600

    Make it read DB from the file

--- src/server.js
+++ src/server.js
@@ -8,28 +8,37 @@ const mock = require('./api.mock')
 const PORT = 3001
 const DB_PATH = 'db/surveys.db.json'
 
+function proceedNoDB (res, e) {
+  console.error('Reading file error:', e)
+  res.json(mock)
+}
+
 app.use(express.json())
 
-app.get('/api', function (req, res) {
-  res.json(mock)
+app.get('/api', function (req, res, next) {
+  try {
+    if (!fs.existsSync(DB_PATH)) {
+      proceedNoDB(res, 'No DB file!')
+      return
+    }
+    const data = fs.readFileSync(DB_PATH)
+    res.json(JSON.parse(data))
+  } catch (e) {
+    proceedNoDB(res, e)
+  }
 })
 
 app.post('/api', function (req, res) {
   try {
-    fs.writeFile(
-      DB_PATH,
-      JSON.stringify(req.body, null, 2),
-      (err) => {
-        if (err) {
-          console.error('err', err)
-          res
-            .status(500)
-            .json({ status: 'error', description: err })
-        }
-        res.json({ status: 'success' })
-      })
+    fs.writeFileSync(DB_PATH, JSON.stringify(req.body, null, 2))
+    res.json({ status: 'success' })
   } catch (e) {
-    // NB It doesn't showed in console, wrap fs into Promise
+    if (e) {
+      console.error('Writing file error:', e)
+      res
+        .status(500)
+        .json({ status: 'error', description: e })
+    }
   }
 })
 
